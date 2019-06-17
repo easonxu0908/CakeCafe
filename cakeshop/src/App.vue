01eb6274bf28affcd036e1145797e2f0ad0c71f7
eason <easonxu0908@gmail.com>
2019-06-17 13:07:07 +0800

    建立cli

---
+++ cakeshop/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+    <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p
+          class="card-text"
+        >Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style lang='scss'>
+@import "~bootstrap/scss/bootstrap";
+</style>
